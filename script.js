12df5b20675c3508f124da96f0c61aa4d3df110b
Mi-precioso <168559906+Mi-precioso@users.noreply.github.com>
2024-04-30 17:53:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const photoSelect = document.getElementById('photoSelect');
 const photo = document.getElementById('photo');
 const gif = document.getElementById('gif');
+const text = document.querySelector('.container p'); // Selecting the paragraph element
 
 photoSelect.addEventListener('change', function() {
     const selectedOption = this.value;
@@ -9,9 +10,11 @@ photoSelect.addEventListener('change', function() {
         photo.src = 'assets/animated.JPG';
         photo.classList.add('animated');
         gif.style.display = 'none';
+        text.style.display = 'block'; // Displaying the text
     } else {
         photo.src = `assets/${selectedOption}.png`;
         photo.classList.remove('animated');
         gif.style.display = 'none';
+        text.style.display = 'none'; // Hiding the text for other options
     }
 });
