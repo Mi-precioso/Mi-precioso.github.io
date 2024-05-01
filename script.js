fb298f25c45af44c50061a19c419652bc2196c5c
Mi-precioso <168559906+Mi-precioso@users.noreply.github.com>
2024-05-01 14:52:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const photoSelect = document.getElementById('photoSelect');
 const photo = document.getElementById('photo');
 const gif = document.getElementById('gif');
-const text = document.querySelector('.container p'); // Selecting the paragraph element
+const text = document.querySelector('.hidden-text'); // Selecting the paragraph element with class 'hidden-text'
 
 photoSelect.addEventListener('change', function() {
     const selectedOption = this.value;
@@ -10,7 +10,7 @@ photoSelect.addEventListener('change', function() {
         photo.src = 'assets/animated.JPG';
         photo.classList.add('animated');
         gif.style.display = 'none';
-        text.style.display = 'block'; // Displaying the text
+        text.style.display = 'block'; // Displaying the text only when "animated" option is selected
     } else {
         photo.src = `assets/${selectedOption}.png`;
         photo.classList.remove('animated');
