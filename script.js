c1b58d7d20729f5b3f005d44f7990c3032f38f5f
Mi-precioso <168559906+Mi-precioso@users.noreply.github.com>
2024-05-01 14:55:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ photoSelect.addEventListener('change', function() {
     const selectedOption = this.value;
     
     if (selectedOption === 'animated') {
-        photo.src = 'assets/animated.JPG';
+        photo.src = 'Assets/animated.JPG';
         photo.classList.add('animated');
         gif.style.display = 'none';
         text.style.display = 'block'; // Displaying the text only when "animated" option is selected
     } else {
-        photo.src = `assets/${selectedOption}.png`;
+        photo.src = `Assets/${selectedOption}.png`;
         photo.classList.remove('animated');
         gif.style.display = 'none';
         text.style.display = 'none'; // Hiding the text for other options
